13ea0d274d9fdbf999381d5f055361e274bee120
Shen Tang <adatang23@yahoo.com>
2026-08-10 09:56:48 -0400

    Initial commit

---
+++ section6/food-delivery-app/src/app/restaurant-listing/service/restaurant-service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { API_URL_RL } from '../../constants/url';
+import { RestaurantDTO } from '../../shared/model/restaurant';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RestaurantService {
+    private apiUrl = API_URL_RL + '/restaurant/fetchAllRestaurants';
+    
+    constructor(private http: HttpClient) { }
+
+    //fetch data from the backend, RxJS stream
+    getAllRestaurants(): Observable<RestaurantDTO[]> {
+        return this.http.get<RestaurantDTO[]>(`${this.apiUrl}`)
+        .pipe(
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(error: any) {
+        console.error('An error occurred:', error);
+        return throwError(() => new Error(error.message || error));
+    }
+}
